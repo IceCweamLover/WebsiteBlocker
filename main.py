c2a139ca088f748407c2c1d9b2bcc38324a0dc3a
clive <98222803+AAClive@users.noreply.github.com>
2022-02-13 17:59:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+import requests
+import time
+import lxml
+import datetime
+import lxml
+import threading
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import pandas_datareader as web
+import datetime as dt
+from selenium.webdriver.common.by import By
 
 
 
+PATH="whatever your selenuim path is" (hint: mine is C:/Users/clive/Downloads/chromedriver.exe)
 
 
+driver=webdriver.Chrome(PATH)
+driver.maximize_window()
 
-
-
-
-
-
-
-
-
-# MAIN CODE GOES HERE
